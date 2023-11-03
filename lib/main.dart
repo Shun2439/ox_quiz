4f8be6d7e0d480c8a56d72de6ec44d12616532b4
Shun2439 <shunta.kobayashi24@gmail.com>
2023-11-03 12:32:37 +0900

    :white_check_mark:Ready to publish

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,17 @@
 import 'package:flutter/material.dart';
-import 'dart:io';
 import 'dart:async';
-import 'dart:convert';
-
-Future<List<List>> csvImport() async {
-  final File importFile = File('quiz.csv');
-  List<List> importList = [];
-
-  Stream fread = importFile.openRead();
-
-  // Read lines one by one, and split each ','
-  await fread.transform(utf8.decoder).transform(LineSplitter()).listen(
-    (String line) {
-      importList.add(line.split(','));
-    },
-  ).asFuture();
-
-  return Future<List<List>>.value(importList);
-}
+import 'package:csv/csv.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
+}
+
+//csvの読み込み
+Future<List<List<dynamic>>> loadCsvData() async {
+  final csvData = await rootBundle.loadString('quiz.csv');
+  List<List<dynamic>> questions = const CsvToListConverter().convert(csvData);
+  return questions;
 }
 
 class MyApp extends StatelessWidget {
@@ -30,6 +21,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'ox_quiz',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
@@ -48,48 +40,31 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  //ユーザーの解答
+  List<List<dynamic>> questions = [];
+
+  int currentQuestion = 0;
+
+  bool buttonEnable = true;
+
   int userSelect = 2;
 
-  //正解または不正解
-  String ansText = '';
-
-  bool showAnsText = true;
-
-  //問題文
-  var question = <String>[
-    "高専の教室には授業用の黒板が２つある",
-    "高専には書道の授業がある",
-    "高専では授業でなでしこをならう",
-    "高専ではテストで60点未満をとってはいけない",
-  ];
-
-  //問題の答え
-  var answer = <int>[
-    1,
-    0,
-    0,
-    1,
-  ];
-
-  //問題番号
-  int questionNumber = 0;
+  String ansText = "";
 
   int correctCount = 0;
 
-  bool buttonEnable = true;
+  bool showAnsText = true;
 
   bool showEndMessage = false;
 
   checkAnswer() {
-    if (userSelect == answer[questionNumber]) {
+    if (userSelect == questions[currentQuestion][1]) {
       ansText = '正解です！';
       correctCount++;
     } else {
       ansText = '不正解です！';
     }
-    if (questionNumber < question.length - 1) {
-      questionNumber++;
+    if (currentQuestion < questions.length - 1) {
+      currentQuestion++;
     } else {
       buttonEnable = false;
       showAnsText = false;
@@ -98,107 +73,141 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    loadCsvData().then((loadedQuestions) {
+      setState(() {
+        questions = loadedQuestions;
+      });
+    });
+  }
+  // _loadCSV();
+
+  // setState(() {
+  //   for (var i = 0; i < _data.length; i++) {
+  //     question.add(_data[i][0].join(""));
+  //   }
+
+  //   for (var i = 0; i < _data.length; i++) {
+  //     answer.add(_data[i][1]);
+  //   }
+  // });
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              '問題:${questionNumber + 1}/${question.length}',
-              style: TextStyle(
-                fontSize: 40,
-              ),
-            ),
-            Text(
-              '${question[questionNumber]}',
-              style: TextStyle(
-                fontSize: 50,
-              ),
-            ),
-            ButtonBar(
-                alignment: MainAxisAlignment.center,
-                buttonPadding:
-                    EdgeInsets.symmetric(horizontal: 80, vertical: 10),
-                children: [
-                  SizedBox(
-                    height: 100,
-                    width: 300,
-                    child: ElevatedButton(
-                      child: const Text(
-                        '⭕',
-                        style: TextStyle(
-                          fontSize: 40,
-                        ),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.blue,
-                        onPrimary: Colors.white,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        elevation: 100,
-                      ),
-                      onPressed: buttonEnable
-                          ? () {
-                              setState(() {
-                                userSelect = 1;
-                                checkAnswer();
-                              });
-                            }
-                          : null,
-                    ),
+      // appBar: AppBar(
+      //   backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      //   title: Text(widget.title),
+      // ),
+      body: (questions.isEmpty)
+          ? CircularProgressIndicator()
+          : Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                //開始ボタン
+                Text(
+                  '問題:${currentQuestion + 1}/${questions.length}',
+                  style: TextStyle(
+                    fontSize: 40,
                   ),
-                  SizedBox(
-                    height: 100,
-                    width: 300,
-                    child: ElevatedButton(
-                      child: const Text(
-                        '❌',
-                        style: TextStyle(
-                          fontSize: 40,
-                        ),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.red,
-                        onPrimary: Colors.white,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        elevation: 100,
-                      ),
-                      onPressed: buttonEnable
-                          ? () {
-                              setState(() {
-                                userSelect = 0;
-                                checkAnswer();
-                              });
-                            }
-                          : null,
-                    ),
+                ),
+                Text(
+                  '${questions[currentQuestion][0]}',
+                  style: TextStyle(
+                    fontSize: 50,
                   ),
-                ]),
-            if (showAnsText)
-              Text(
-                '$ansText',
-                style: TextStyle(fontSize: 40),
-              ),
-            if (showEndMessage)
-              Text(
-                'クイズが終了しました\nF5を押してください',
-                style: TextStyle(fontSize: 40),
-              ),
-            Text(
-              '正解数:${correctCount}/${question.length}',
-              style: TextStyle(fontSize: 40),
+                ),
+                ButtonBar(
+                    alignment: MainAxisAlignment.center,
+                    buttonPadding:
+                        EdgeInsets.symmetric(horizontal: 80, vertical: 10),
+                    children: [
+                      SizedBox(
+                        height: 100,
+                        width: 300,
+                        child: ElevatedButton(
+                          child: const Text(
+                            '⭕',
+                            style: TextStyle(
+                              fontSize: 40,
+                            ),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.blue,
+                            onPrimary: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            elevation: 100,
+                          ),
+                          onPressed: buttonEnable
+                              ? () {
+                                  setState(() {
+                                    userSelect = 1;
+                                    showAnsText = true;
+                                    checkAnswer();
+                                    Future.delayed(Duration(seconds: 3), () {
+                                      setState(() {
+                                        showAnsText = false;
+                                      });
+                                    });
+                                  });
+                                }
+                              : null,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 100,
+                        width: 300,
+                        child: ElevatedButton(
+                          child: const Text(
+                            '❌',
+                            style: TextStyle(
+                              fontSize: 40,
+                            ),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.red,
+                            onPrimary: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            elevation: 100,
+                          ),
+                          onPressed: buttonEnable
+                              ? () {
+                                  setState(() {
+                                    userSelect = 0;
+                                    showAnsText = true;
+                                    checkAnswer();
+                                    Future.delayed(Duration(seconds: 3), () {
+                                      setState(() {
+                                        showAnsText = false;
+                                      });
+                                    });
+                                  });
+                                }
+                              : null,
+                        ),
+                      ),
+                    ]),
+                if (showAnsText)
+                  Text(
+                    '$ansText',
+                    style: TextStyle(fontSize: 40),
+                  ),
+                if (showEndMessage)
+                  Text(
+                    'クイズが終了しました\nF5を押してください',
+                    style: TextStyle(fontSize: 40),
+                  ),
+                Text(
+                  '正解数:${correctCount}/${questions.length}',
+                  style: TextStyle(fontSize: 40),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
     );
   }
 }
